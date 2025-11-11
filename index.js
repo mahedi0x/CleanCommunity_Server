08adb98b46226ad9efa74ed4e3fe65dfcda98b81
Mahedi0x <mahedih.cse@gmail.com>
2025-11-11 10:11:22 +0600

    connected mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,10 +9,39 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// clean-community-db
+// M6Pc77y0kXprykCx
+
+const uri =
+  "mongodb+srv://clean-community-db:M6Pc77y0kXprykCx@cluster0.xtjchvv.mongodb.net/?appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World CleanCommunity!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`CleanCommunity app listening on port ${port}`);
 });
