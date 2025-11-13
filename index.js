0913536b34870ff2ad1c42697be6b4ecc7e9900e
Mahedi0x <mahedih.cse@gmail.com>
2025-11-13 23:11:27 +0600

    complete server

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const db = client.db("clean_community_db");
     const issuesCollection = db.collection("issues");
     const usersCollection = db.collection("users");
@@ -160,7 +160,7 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
